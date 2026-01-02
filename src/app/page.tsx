2a388d130af7a1fc3447b7c0a884714f565070ba
pouriaahmadi4 <pouriaspotify1@gmail.com>
2026-01-02 17:26:39 +0330

    Add workspace directory to .gitignore, enhance globals.css with additional theme colors and styles, implement Vazirmatn font in layout, create header component, and update homepage content and styling for testing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">
-        Platform Core – Phase 1 Ready
+    <main className="flex flex-col min-h-screen items-center justify-center bg-dark-down text-highlight p-900">
+      <h1 className="text-900 font-sans text-4xl font-medium leading-tight tracking-md text-mint">
+        دانجو، کهکشان دانستنی‌های ماست!
       </h1>
+      <p className="max-w-short text-4xl font-sans font- text-lilac">
+        به دنیای یادگیری خوش آمدی! اینجا جاییه که می‌تونی مهارت‌های زبان
+        انگلیسی، مهارت‌های نرم و اقتصاد رفتاری رو به صورت تعاملی یاد بگیری
+      </p>
     </main>
-  )
+  );
 }

9ca73fbd7b4069efcf29d5c69cfb833a2b083c55
pouriaahmadi4 <pouriaspotify1@gmail.com>
2026-04-22 21:01:38 +0330

    creating ui folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import "./ui/globals.css";
 import { Vazirmatn } from "next/font/google";
-import "./globals.css";
+// import { inter } from "./ui/fonts";
+
+export const metadata: Metadata = {
+  title: "Dawnjoo – English Classes, Soft Skills & Behavioral Economics",
+  description:
+    "Interactive learning for English mastery, soft skills, and behavioral insights.",
+};
 
 const vazirmatn = Vazirmatn({
   subsets: ["arabic"],
@@ -8,12 +15,6 @@ const vazirmatn = Vazirmatn({
   display: "swap",
   weight: ["400", "500", "700"],
 });
-
-export const metadata: Metadata = {
-  title: "Dawnjoo – English Classes, Soft Skills & Behavioral Economics",
-  description:
-    "Interactive learning for English mastery, soft skills, and behavioral insights.",
-};
 
 export default function RootLayout({
   children,
@@ -28,3 +29,18 @@ export default function RootLayout({
     </html>
   );
 }
+
+//? we can use {inter.className} instead of the primary font
+//? to use just uncomment code below and import statement from fonts.tsx file
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${inter.className} antialiased`}>{children}</body>
+//     </html>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center bg-dark-down text-highlight p-900">
+    <main className="flex flex-col min-h-screen bg-dark-down text-highlight p-6">
       <h1 className="text-900 font-sans text-4xl font-medium leading-tight tracking-md text-mint">
         دانجو، کهکشان دانستنی‌های ماست!
       </h1>
-      <p className="max-w-short text-4xl font-sans font- text-lilac">
+      <p className="max-w-short text-4xl font-sans font-medium text-lilac">
         به دنیای یادگیری خوش آمدی! اینجا جاییه که می‌تونی مهارت‌های زبان
         انگلیسی، مهارت‌های نرم و اقتصاد رفتاری رو به صورت تعاملی یاد بگیری
       </p>
